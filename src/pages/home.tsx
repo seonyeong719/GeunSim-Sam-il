4c518ef636c227f15e3070cf4b2262855574031c
seonyeong719 <sy904280@gmail.com>
2023-08-23 20:05:39 +0900

    :lipstick: Design: 홈페이지 일부 css 추가

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,25 @@
+import { BasicSetting } from "@styles/common";
+import { styled } from "styled-components";
+
 function HomePage() {
   return (
-    <>
-      <div>home Page</div>
-    </>
+    <S.Wrap>
+      <S.Wrapper>home Page</S.Wrapper>
+    </S.Wrap>
   );
 }
 export default HomePage;
+
+const Wrap = styled.div`
+  width: 100%;
+`;
+
+const Wrapper = styled.div`
+  ${BasicSetting}
+  background-color: red;
+`;
+
+const S = {
+  Wrap,
+  Wrapper,
+};
