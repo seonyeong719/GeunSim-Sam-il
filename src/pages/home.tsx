59238855f6af8137d1f839154b1b59ab638aa028
seonyeong719 <sy904280@gmail.com>
2023-08-29 23:20:20 +0900

    :lipstick: Design: 메인 페이지 aos추가해서 디자인 틀

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,32 @@
 import { BasicSetting } from "@styles/common";
+import { useEffect } from "react";
 import { styled } from "styled-components";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function HomePage() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <S.Wrap>
-      <S.Wrapper>home Page</S.Wrapper>
+      <S.Wrapper>
+        <S.Img1Wrap>
+          <S.Img1 data-aos="fade-right" src="../../public/Img/운동고민.png" />
+          <div>이미지1 소개글</div>
+        </S.Img1Wrap>
+        <S.Img2Wrap>
+          <div>이미지1 소개글</div>
+          <S.Img2 data-aos="fade-left" src="../../public/Img/스트레스 관리.png" />
+        </S.Img2Wrap>
+        <S.Img3Wrap>
+          <S.Img3 data-aos="fade-up" data-aos-duration="3000" src="../../public/Img/로고.png" />
+          <div data-aos="fade-up" data-aos-duration="3000">
+            프로젝트 소개 글
+          </div>
+        </S.Img3Wrap>
+      </S.Wrapper>
     </S.Wrap>
   );
 }
@@ -16,10 +38,57 @@ const Wrap = styled.div`
 
 const Wrapper = styled.div`
   ${BasicSetting}
-  background-color: red;
+  height: 30rem;
+  display: flex;
+  flex-direction: column;
+  padding-top: 5rem;
+`;
+
+const Img1Wrap = styled.div`
+  padding: 8rem 0;
+  display: flex;
+`;
+
+const Img1 = styled.img`
+  width: 40rem;
+  height: 30rem;
+`;
+
+const Img2Wrap = styled.div`
+  display: flex;
+  justify-content: end;
+  padding: 12rem 0;
+`;
+
+const Img2 = styled.img`
+  width: 40rem;
+  height: 30rem;
+`;
+
+const Img3Wrap = styled.div`
+  padding: 12rem 0;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Img3 = styled.img`
+  width: 20rem;
+  height: 20rem;
+  border: 1px solid black;
+  display: flex;
+  justify-content: center;
 `;
 
 const S = {
   Wrap,
   Wrapper,
+  Img1Wrap,
+  Img1,
+  Img2Wrap,
+  Img2,
+  Img3Wrap,
+  Img3,
 };
